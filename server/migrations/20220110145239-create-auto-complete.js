40e829e8089504fee423aa816d94a68188df5063
GWANGUIAN <bbaa3218@gmail.com>
2022-01-11 18:28:27 +0900

    Feat: 이미지 업로드 구현 및 설정 관련 리듀서 생성

--- server/migrations/20220110145239-create-auto-complete.js
+++ server/migrations/20220110145239-create-auto-complete.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        references:{model: 'User', key: 'id'}
+        references:{model: 'Users', key: 'id'}
       },
       word: {
         type: Sequelize.STRING
