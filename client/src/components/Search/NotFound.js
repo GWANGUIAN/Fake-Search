94d57423e6f8871b95d621286436056706d7f4a1
GWANGUIAN <bbaa3218@gmail.com>
2022-01-15 02:23:58 +0900

    Fix: 빈 문자열 검색 라우터 설정

--- client/src/components/Search/NotFound.js
+++ client/src/components/Search/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound({word}) {
   return <div className='notfound-container'>
-      <div className='text-title'><span id='text-word'>{`'${word}'`}</span><span>에 해당하는 검색결과가 없습니다.</span></div>
+      <div className='text-title'><span id='text-word'>{word!==undefined ? `'${word}'` : `' '`}</span><span>에 해당하는 검색결과가 없습니다.</span></div>
       <ul>
           <li>단어의 철자가 정확한지 확인해 보세요.</li>
           <li>한글을 영어로 혹은 영어를 한글로 입력했는지 확인해 보세요.</li>
